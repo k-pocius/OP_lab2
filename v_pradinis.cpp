6b14021b090f3ba7114c0fae7c781415132c145b
k-pocius <pockristijonas@gmail.com>
2025-02-09 21:21:03 +0200

    (4) fixed skaiciavimo budo pasirinkimas

--- v_pradinis.cpp
+++ v_pradinis.cpp
@@ -22,10 +22,16 @@ int m; //mokiniu sk.
 int main(){
     char a;
 
-    skaitom();
+//    skaitom();
 
     cout << "pasirinkite skaičiavimo būdą vidurkis(v)/mediana(m): ";
     cin >> a;
+    while(!(a == 'v' || a == 'm')){
+        cout << "klaida, pasirinkite v arba m: ";
+        cin.clear();
+        cin >> a;
+        cin.ignore(123, '\n');
+    }
     if(a == 'v'){
         vidurkis();
     } else if(a == 'm'){
