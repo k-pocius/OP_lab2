8211d069c03e0f815593c52596dffb794653b1ec
k-pocius <pockristijonas@gmail.com>
2025-02-12 12:30:56 +0200

    (3) sukurtas cpp su vektoriais

--- V01vect.cpp
+++ V01vect.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
@@ -11,12 +12,13 @@ void spausdinam(char a);
 struct Student {
     string name; //vardas
     string surn; //pavarde
-    int nd[25], egz; //nd, egzaminu rez
+    vector<int> nd; //nd rezultatai 
+    int egz; //egzaminu rez
     double vid; //galutinis vidurkis
     int n; //nd kiekis
-}C[20];
+};
 
-int m; //mokiniu sk., namu darbu kiekis
+vector<Student> students;
 
 int main(){
     char a;
@@ -44,24 +46,33 @@ void skaitom(){
     
     bool testi = true;
     int i = 0;    
+    Student temp;
     while(testi){
         char teesti;
 
+        //mokinio vardas pavarde
         cout << "įveskite mokinio vardą: ";
-        cin >> C[i].name;
+        cin >> temp.name;
         cout << "įveskite mokinio pavardę: ";
-        cin >> C[i].surn;
+        cin >> temp.surn;
 
+
+        temp.nd.clear();
+
+        //nd rezultatai
         bool testi2 = true;
         int j = 0;
         while(testi2){
             char teesti2;
+            int nd_result;
             cout << "įveskite " << j+1 << " namų darbo rezultatą: ";
-            while(!(cin >> C[i].nd[j])){
+            while(!(cin >> nd_result)){
                 cout << "klaida, įveskite skaičių: ";
                 cin.clear();
                 cin.ignore(123, '\n');
             }
+            temp.nd.push_back(nd_result);
+
             cout << "ar norite pridėti daugiau rezultatų? (t/n): ";
             cin >> teesti2;
             while(!(teesti2 == 't' || teesti2 == 'n')){
@@ -70,23 +81,24 @@ void skaitom(){
                 cin >> teesti2;
                 cin.ignore(123, '\n');
             }
-        if(teesti2 == 'n'){
-            testi2 = false;
-        } else testi2 = true;
-        j++;
-        C[i].n = j;
-        if(j == 20) break;
+            if(teesti2 == 'n'){
+                testi2 = false;
+            } else testi2 = true;
+            j++;
+            if(j == 20) break;
         }
 
 
+        //egzamino rezultatas
         cout << "įveskite egzamino rezultatą: ";
-        while(!(cin >> C[i].egz)){
+        while(!(cin >> temp.egz)){
             cout << "klaida, įveskite skaičių: ";
             cin.clear();
             cin.ignore(123, '\n');
         }
+        students.push_back(temp);
         
-
+        
         cout << "ar norite pridėti daugiau mokinių? (t/n): ";
         cin >> teesti;
         while(!(teesti == 't' || teesti == 'n')){
@@ -99,35 +111,34 @@ void skaitom(){
             testi = false;
         }
         i++;
-        m = i;
         if(i == 15) break;
     }
 }
 
 void vidurkis(){
-    for(int i = 0; i < m; i++){
+    for(int i = 0; i < students.size(); i++){
         double sum = 0;
-        for(int j = 0; j < C[i].n; j++){
-            sum += C[i].nd[j];
+        for(int j = 0; j < students[i].nd.size(); j++){
+            sum += students[i].nd[j];
         }
-        C[i].vid = (sum / C[i].n)*0.4 + (C[i].egz*0.6);
+        students[i].vid = (sum / students[i].nd.size())*0.4 + (students[i].egz*0.6);
     }
 }
 
 void mediana(){
 
     //nd rezultatu rikiavimas didejimo tvarka
-    for (int i = 0; i < m; i++) {
-        sort(C[i].nd, C[i].nd + C[i].n); 
+    for (int i = 0; i < students.size(); i++) {
+        sort(students[i].nd.begin(), students[i].nd.end()); 
     }
 
     //medianos skaiciavimas
-    for(int i = 0; i < m; i++){
-        if(C[i].n % 2 == 0){
-            C[i].vid = ((C[i].nd[C[i].n/2] + C[i].nd[(C[i].n/2)-1]) / 2.0)*0.4 + (C[i].egz*0.6);
+    for(int i = 0; i < students.size(); i++){
+        if(students[i].nd.size() % 2 == 0){
+            students[i].vid = ((students[i].nd[students[i].nd.size()/2] + students[i].nd[students[i].nd.size()/2 - 1]) / 2.0)*0.4 + (students[i].egz*0.6);
         } 
         else {
-            C[i].vid = C[i].nd[C[i].n/2]*0.4 + (C[i].egz*0.6);
+            students[i].vid = students[i].nd[students[i].nd.size()/2]*0.4 + (students[i].egz*0.6);
         }
     }
 }
@@ -144,7 +155,7 @@ void spausdinam(char a){
 
     cout << fixed << setprecision(2);
 
-    for(int i = 0; i < m; i++){
-        cout << left << setw(15) << C[i].surn << setw(15) << C[i].name << setw(20) << C[i].vid << endl;
+    for(int i = 0; i < students.size(); i++){
+        cout << left << setw(15) << students[i].surn << setw(15) << students[i].name << setw(20) << students[i].vid << endl;
     }
 }
