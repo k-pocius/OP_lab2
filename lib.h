6c549715fbe3f3944497aa78c886dfabfeef9300
k-pocius <pockristijonas@gmail.com>
2025-02-28 09:04:40 +0200

    2, sukurtas header lib failas

---
+++ lib.h
@@ -0,0 +1,30 @@
+#ifndef LIB_H
+#define LIB_H
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include <time.h>
+
+using std:: string;
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::ofstream;
+using std::setw;
+using std::left;
+using std::fixed;
+using std::setprecision;
+using std::sort;
+using std::swap;
+using std::numeric_limits;
+using std::streamsize;
+
+
+#endif
